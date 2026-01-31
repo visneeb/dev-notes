c25e8b408a30ba5f6d04bb4dafd1b2249cc6c789
VisneeB <visinee@gmail.com>
2026-01-31 16:44:11 +0700

    fix(validation): update error message for minimum length rule

--- src/validations/common/rules.ts
+++ src/validations/common/rules.ts
@@ -2,7 +2,7 @@ export const emailRule = () => (value: string) =>
   /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? null : "Email must be valid email";
 
 export const minLength = (len: number) => (value: string) =>
-  value.length >= len ? null : `must be at least ${len} characters`;
+  value.length >= len ? null : `Password must be at least ${len} characters`;
 
 export const hasNumber = () => (value: string) =>
   /\d/.test(value) ? null : "Must contain a number";
